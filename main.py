746bf05622762589f796890a29b9c4eb622d48a2
unknown <bhavaybhatia1153@gmial.com>
2026-08-19 00:16:07 +0530

    new project

---
+++ main.py
@@ -0,0 +1,11 @@
+import os 
+def arrange_files(files,ext):
+    files_with_ext=[file for file in files if file.endswith(ext)]
+    print(files_with_ext)
+
+    for i,file in enumerate(files_with_ext):
+          os.rename(file,f"iamges/photo.{i+1}{ext}")
+
+if __name__=="__main__":
+    files=os.listdir()
+    arrange_files(files,".pdf")
